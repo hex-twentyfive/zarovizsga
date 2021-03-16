c7b0b0c7a09a091d096a2625c09bf521352d55a1
hex-twentyfive <czinky.gyorgy@gmail.com>
2021-03-16 18:20:07 +0100

    first task completed

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,5 +1,22 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DigitsCounter {
+
+    public int getCountOfDigits(String s) {
+        if (s == null) {
+            return 0;
+        }
+        List<String> result = new ArrayList<>();
+        String[] splitS = s.split("");
+        for (int i = 0; i< splitS.length; i++) {
+            if (!result.contains(splitS[i]) && splitS[i].matches("[0-9]+")) {
+                result.add(splitS[i]);
+            }
+        }
+        return result.size();
+    }
 
 }
